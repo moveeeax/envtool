cfa4efdc4bcecfcfd7d07c62395a607b9ba58ad4
Michael Tarassov <michael@tarassov.me>
2021-01-25 22:44:17 +0300

    Add validate command

--- cmd/root.go
+++ cmd/root.go
@@ -17,6 +17,7 @@ func newRootCmd() *cobra.Command {
 		newMergeCmd(),
 		newExportCmd(),
 		newDiffCmd(),
+		newValidateCmd(),
 	)
 	return root
 }
---
+++ cmd/validate.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/moveeeax/envtool/internal/env"
+	"github.com/spf13/cobra"
+)
+
+func newValidateCmd() *cobra.Command {
+	var required string
+	cmd := &cobra.Command{
+		Use:   "validate [flags] FILE",
+		Short: "Check that required keys are present and non-empty",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			doc, err := env.ParseFile(args[0])
+			if err != nil {
+				return err
+			}
+			keys := strings.Split(required, ",")
+			violations := env.Validate(doc, env.RulesFromKeys(keys))
+			if len(violations) == 0 {
+				return nil
+			}
+			for _, v := range violations {
+				fmt.Fprintln(os.Stderr, v)
+			}
+			return fmt.Errorf("%d validation error(s)", len(violations))
+		},
+	}
+	cmd.Flags().StringVar(&required, "required", "", "comma-separated required keys")
+	return cmd
+}
